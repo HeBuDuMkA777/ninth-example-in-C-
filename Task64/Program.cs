5e34aceb4fa1dfc14b5788267342623bb9d80058
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-26 23:20:39 +0300

    задача 64 готова(рекурсия)

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число - начало отрезка: ");
+int numStart = int.Parse(ReadLine());
+WriteLine("Введите число - конец отрезка: ");
+int numEnd = int.Parse(ReadLine());
+WriteLine(GetNumbers(numStart, numEnd));
+
+
+
+
+
+string GetNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    {
+        return (start + " " + GetNumbers(start + 1, end));
+    }
+}
