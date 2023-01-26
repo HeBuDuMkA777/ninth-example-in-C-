080276257bf9a8e2865b3452e16ef6b03f5bbf5e
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-26 23:31:09 +0300

    Победил задачу 66 (тоже рекурсивно)

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число - начало отрезка: ");
+int numStart = int.Parse(ReadLine());
+WriteLine("Введите число - конец отрезка: ");
+int numEnd = int.Parse(ReadLine());
+WriteLine(GetSumNumbers(numStart, numEnd));
+
+int GetSumNumbers(int start, int end)
+{
+    if (start == end) return + start;
+    {
+        return(start + GetSumNumbers(start + 1, end));
+    }
+}
