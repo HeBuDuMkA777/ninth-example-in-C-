a0245fc60bee75fbcb3b497d9e9a357879b8f2d0
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-26 23:51:08 +0300

    выполнил задание 68, тоже рекурсия

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int numM = 2; //Условие задачи
+int numN = 3; // Условие задачи
+WriteLine(GetNumAkkerman(numM, numN));
+
+
+int GetNumAkkerman(int numberA, int numberB)
+{
+    if (numberA == 0) return numberB + 1;
+    else if (numberB == 0 & numberA > 0) return GetNumAkkerman(numberA - 1, 1);
+    else if (numberA > 0 & numberB > 0) return GetNumAkkerman(numberA - 1, GetNumAkkerman(numberA, numberB - 1));
+    else return -1;
+}
